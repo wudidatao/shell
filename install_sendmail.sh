2890f37f1bb7b95c8a8410c1c2a1515a389a59a3
wudidatao <82302925@qq.com>
2019-03-31 15:48:19 +0800

    Create install_sendmail.sh

---
+++ install_sendmail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+host_ip=192.168.1.52
+host_name=public-1.52
+host_domain_name=public-1.52.com
+
+yum install sendmail
+
+sed -i "s/Addr=127.0.0.1/Addr=$host_ip/g" /etc/mail/sendmail.cf
+
+echo "$host_ip $host_name $host_domain_name " >> /etc/hosts
+
+systemctl enable sendmail.service
+systemctl start sendmail.service
